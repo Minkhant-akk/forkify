1ad01660a8d8fb947c5795c9631b3bf3dc46be97
minkhant-akk <sunnycornet123@gmail.com>
2023-10-21 15:18:05 +0630

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 15;
+export const KEY = '935089e9-6d54-46a5-9162-b2b0f5a67e6e';
+export const MODAL_CLOSE_SEC = 2.5;
